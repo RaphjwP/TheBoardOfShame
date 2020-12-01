db59e1413139642177bc87d756723aec7d7ed36f
Jaewon Park <raph.ej.p@outlook.com>
2020-12-01 17:29:16 -0500

    DB Implementation

---
+++ TheBoardOfShame/Model/Chore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBoardOfShame.Model
+{
+    public class Chore
+    {
+        public int Id { get; set; }
+
+        public List<User> Users { get; set; }
+    }
+}
---
+++ TheBoardOfShame/Model/Database.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheBoardOfShame.Model
+{
+    public class Database : DbContext
+    {
+        public Database()
+        {
+
+        }
+
+        public Database(DbContextOptions<Database> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Chore> Chore { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source=(localdb)\\ProjectsV13;Initial Catalog=BoardOfShameDB;Integrated Security=True;");
+            //Data Source=(localdb)\ProjectsV13;Initial Catalog=master;Integrated Security=True;
+            //Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+            //optionsBuilder.UseS
+            //optionsBuilder.UseSqlServer("");
+        }
+    }
+}
