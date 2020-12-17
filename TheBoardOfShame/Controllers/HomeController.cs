2443d01bc09746215437c4c81fb6c985c4b7b350
Mike Komor <komormi@shernet.sheridancollege.ca>
2020-12-17 17:29:51 -0500

    added chores/editchoresview

--- TheBoardOfShame/Controllers/HomeController.cs
+++ TheBoardOfShame/Controllers/HomeController.cs
@@ -9,13 +9,44 @@ namespace TheBoardOfShame.Controller
 {
     public class HomeController : Microsoft.AspNetCore.Mvc.Controller
     {
+
+        private Database _database;
         public IActionResult Index()
         {
             return View();
         }
 
+
+        public HomeController(Database database)
+        {
+            _database = database;
+        }
+
         public IActionResult ValidateUser(User user)
         {
+            return View("MainPage");
+        }
+
+        [Route("Home/Edit/{id}")]
+        public IActionResult EditChores(int id)
+        {
+            var chore = _database.Chore.Find(id);
+
+            return View(chore);
+        }
+
+        [HttpPost]
+        public IActionResult EditChores(Chore chore)
+        {
+            var choreToChange = _database.Chore.Find(chore.Id);
+            a
+            choreToChange.ChoreDescription = chore.ChoreDescription;
+            choreToChange.ChoreType = chore.ChoreType;
+            choreToChange.ChoreWeight = chore.ChoreWeight;
+
+            _database.Chore.Update(choreToChange);
+            _database.SaveChanges();
+
             return View("MainPage");
         }
     }
